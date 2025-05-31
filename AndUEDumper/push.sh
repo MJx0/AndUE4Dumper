5c1944f102bd970f370228841aeccd466514477e
MJx0 <32413318+MJx0@users.noreply.github.com>
2025-05-31 21:24:27 +0000

    Update push.sh

--- AndUEDumper/push.sh
+++ AndUEDumper/push.sh
@@ -2,16 +2,16 @@
 
 echo pushing dumper binaries...
 
-adb push libs/arm64-v8a/UEDump3r_arm64 /data/local/tmp
+adb push build/libs/arm64-v8a/UEDump3r_arm64 /data/local/tmp
 adb shell "su -c 'chmod 755 /data/local/tmp/UEDump3r_arm64'"
 
-adb push libs/armeabi-v7a/UEDump3r_arm /data/local/tmp
+adb push build/libs/armeabi-v7a/UEDump3r_arm /data/local/tmp
 adb shell "su -c 'chmod 755 /data/local/tmp/UEDump3r_arm'"
 
-adb push libs/x86/UEDump3r_x86 /data/local/tmp
+adb push build/libs/x86/UEDump3r_x86 /data/local/tmp
 adb shell "su -c 'chmod 755 /data/local/tmp/UEDump3r_x86'"
 
-adb push libs/x86_64/UEDump3r_x86_64 /data/local/tmp
+adb push build/libs/x86_64/UEDump3r_x86_64 /data/local/tmp
 adb shell "su -c 'chmod 755 /data/local/tmp/UEDump3r_x86_64'"
 
 :: test on arm64 device
